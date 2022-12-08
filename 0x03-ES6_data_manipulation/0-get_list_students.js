3eb39994b42adf75631980db05e6a6a1f2e07ba8
Edheramirez <https://github.com/Edheramirez>
2022-12-07 23:48:00 -0500

    fix syntax

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,5 +1,8 @@
-export default function getListStudentIds(studentsList) {
-    if (!Array.isArray(studentsList)) return [];
-    const studentsIds = studentsList.map((student) => student.id);
-    return studentsIds;
+export default function getListStudents() {
+    const students = [
+      { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+      { id: 2, firstName: 'James', location: 'Columbia' },
+      { id: 5, firstName: 'Serena', location: 'San Francisco' },
+    ];
+    return students;
   }
